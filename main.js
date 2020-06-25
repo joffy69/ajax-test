5c6b8c8fcf97a6db95c656a8be66fd49b0017b1a
joffy69 <jcwgibbs@gmail.com>
2020-06-25 18:20:50 +0000

    finally working - remember to hard reset if things don't appeat to work

--- main.js
+++ main.js
@@ -1,43 +1,57 @@
-const baseURL ="https://ci-swapi.herokuapp.com/api/";
-
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+//a function to request data from a json supplying server
 function getData(type, cb) {
   var xhr = new XMLHttpRequest();
-
-  xhr.open("GET", baseURL + type + "/");
-  xhr.send();
 
   xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       cb(JSON.parse(this.responseText));
     }
   };
+
+  xhr.open("GET", baseURL + type + "/");
+  xhr.send();
+  
 }
 
-function getTableHeaders(obj){
-    var getTableHeaders =[];
 
-    Object.keys(obj).forEach(function(key){
-        tableHeaders.push(`<td>${key}</td>`);
-    });
 
-    return `<tr>${tableHeaders}</tr>`;
-    
+function getTableHeaders(obj) {
+  var tableHeaders = [];
+
+  Object.keys(obj).forEach(function (key) {
+    tableHeaders.push(`<td>${key}</td>`);
+  });
+
+  return `<tr>${tableHeaders}</tr>`;
+  
 }
-function writeToDocument(type){
-    var el=document.getElementById("data");
-    el.innerHTML=""; /*clear */
-    getData(type, function(data){
-        console.dir(data);
-        data=data.results;
-        data.forEach(function(item){
-            Object.keys(item).forEach(function(key){
-                console.log(key);
-            });
-             el.innerHTML += "<p>"+item.name+"</p>";
-         });
-            
-       
+
+function writeToDocument(type) {
+
+  var tableRows = [];
+  var el = document.getElementById("data");
+
+  //el.innerHTML = ""; /*clear */
+
+  getData(type, function (data) {
+    data = data.results;
+    var tableHeaders = getTableHeaders(data[0]);
+
+    data.forEach(function (item) {
+      var dataRow = [];
+      Object.keys(item).forEach(function (key) {
+          var rowData=item[key].toString();
+          var truncatedData=rowData.substring(0,15);
+        dataRow.push(`<td>${truncatedData}</td>`);
+        //console.log(key);
+      });
+      tableRows.push(dataRow);
+      // el.innerHTML += "<p>" + item.name + "</p>";
     });
+
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+  });
 }
 /*function printDataToConsole(data){
     console.log(data);
