bbdc4ac2bd22ec097c06771d51ddf8d7c27c724b
joffy69 <jcwgibbs@gmail.com>
2020-06-26 16:40:58 +0000

    fix commas

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function writeToDocument(url) {
       // el.innerHTML += "<p>" + item.name + "</p>";
     });
 
-    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
   });
 }
 
